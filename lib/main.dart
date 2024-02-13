235833f95fee86210fec981ab4ca1eecb741cae8
Omid Shojaeian Zanjani <46863450+omidshz100@users.noreply.github.com>
2024-02-13 12:07:27 +0100

    MyHome  changed to MyHomePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:music_player/pages/map_page.dart';
+import 'package:music_player/pages/MyHomePage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,11 +13,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MapPage(),
+      home: const MyHomePage(),
     );
   }
 }
